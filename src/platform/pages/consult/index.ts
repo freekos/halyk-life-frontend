ca72566746f70a53c2ac14e827b20743e8d6739d
Gaziz <gazizbolat2@gmail.com>
2024-01-27 14:46:24 +0600

    feat: add initial routing and pages to platform

---
+++ src/platform/pages/consult/index.ts
@@ -0,0 +1,1 @@
+export { ConsultPage } from './page'
---
+++ src/routing/index.tsx
@@ -0,0 +1,14 @@
+import { ConsultPage } from '@/platform/pages/consult'
+import { HomePage } from '@/platform/pages/home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export function Routing() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' Component={HomePage} />
+				<Route path='/consult' Component={ConsultPage} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
