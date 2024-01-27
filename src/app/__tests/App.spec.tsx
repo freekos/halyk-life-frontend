cbc40d80db8c3e7141c2fbfbee34b827c42f174b
Gaziz <gazizbolat2@gmail.com>
2024-01-27 13:37:19 +0600

    refactor: remove unecessary comments

--- src/app/__tests/App.spec.tsx
+++ src/app/__tests/App.spec.tsx
@@ -1,14 +1,5 @@
-import { render, screen } from '@testing-library/react'
-
-import { App } from '../App'
-
 describe('App', () => {
 	it('should render app', () => {
 		expect(true).toBeTruthy()
-		// render(<App />)
-
-		// const headingElement = screen.getByText(/Welcome to React + Vite + SWC template/)
-
-		// expect(headingElement).toBeInTheDocument()
 	})
 })
