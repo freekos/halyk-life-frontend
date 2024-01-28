83855dd2d7078f7be15dce92681ec184300f2c0d
Gaziz <gazizbolat2@gmail.com>
2024-01-28 08:03:19 +0600

    feat: add router

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,14 +1,17 @@
 import { ConsultPage } from '@/platform/pages/consult'
 import { HomePage } from '@/platform/pages/home'
+import { RouterProvider } from '@/shared/router'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export function Routing() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' Component={HomePage} />
-				<Route path='/consult' Component={ConsultPage} />
-			</Routes>
+			<RouterProvider>
+				<Routes>
+					<Route path='/' Component={HomePage} />
+					<Route path='/consult' Component={ConsultPage} />
+				</Routes>
+			</RouterProvider>
 		</BrowserRouter>
 	)
 }
